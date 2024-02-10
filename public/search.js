72c80b7519c083128fdfbf1ff14065b14f7901cf
Felipe Beskow <felipebeskow@outlook.com>
2024-02-09 23:15:52 -0300

    corrigido url artistas

--- public/search.js
+++ public/search.js
@@ -3,7 +3,7 @@ const playlistContainer = document.getElementById("result-playlists");
 const searchInput = document.getElementById("search-input");
 
 function requestApi(searchTerm) {
-  fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
+  fetch(`https://imersao-spotify-felipe-beskow.onrender.com/artists?name_like=${searchTerm}`)
     .then((response) => response.json())
     .then((results) => displayResults(results));
 }
